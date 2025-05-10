20ebd798aa6119ac45d4f9bff9bdab39c1f4d30b
Bruno Sotto <bruno@sotto.com.br>
2025-05-10 20:27:51 -0300

    teste

---
+++ teste.js
@@ -0,0 +1,29 @@
+import Fastify from 'fastify';
+import fastifyWs from 'fastify-ws';
+
+const fastify = Fastify();
+
+fastify.register(fastifyWs);
+
+fastify.ready(err => {
+    if (err) throw err;
+
+    fastify.ws.on('connection', (socket, req) => {
+        console.log('Novo cliente conectado');
+
+        socket.on('message', message => {
+            console.log(`Mensagem recebida: ${message}`);
+            socket.send(`Echo: ${message}`);
+        });
+
+        socket.on('close', () => {
+            console.log('Cliente desconectado');
+        });
+    });
+
+    console.log('Servidor WebSocket rodando na porta 8080');
+});
+
+fastify.listen({ port: 8080 }).then(() => {
+    console.log('Fastify está pronto!');
+});
